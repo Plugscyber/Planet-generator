a00d0f4153ad0ba52d35e1d7f70adf7597b03d70
Plugs <95036760+Plugscyber@users.noreply.github.com>
2022-11-07 21:05:30 +0000

    Updates
    Moon details, more plant details and colour. Better descriptions of barren planets

--- Javascript.js
+++ Javascript.js
@@ -54,6 +54,7 @@ function generate() {
           break;
         case 16:
           consonant.push("n");
+          break;
         case 17:
           consonant.push("o");
           break;
@@ -98,7 +99,9 @@ function generate() {
       }
     }
     //and I know they're not all vowels.
+    //but I can't be bothered to rename.
     let vNum = Math.floor(Math.random() * (11 - 1) + 1);
+    //make one 'vowel'
     switch (vNum) {
       case 1:
         vowel = "ar";
@@ -131,7 +134,7 @@ function generate() {
         vowel = "ee";
         break;
     }
-    //put together the consonants and vowel into a planet name
+    //put together the 'consonants' and 'vowel' into a planet name
     let result = consonant[0] + vowel + consonant[1] + consonant[2];
     //then capitalise the first letter
     let capsResult = result.charAt(0).toUpperCase() + result.slice(1);
@@ -142,26 +145,39 @@ function generate() {
   //store the function value of planet name in the name variable for later use.
   var name = planet();
 
-  //cleaned up with a lookup table.
+  //moon generation.
   function moonNum() {
-    let num = Math.floor(Math.random() * (4 - 1) + 1);
-    const moonTable = {
+    let numM = Math.floor(Math.random() * (4 - 1) + 1);
+    const numTable = {
       1: " has one moon.",
       2: " has two moons.",
       3: " has three moons."
     };
-    document.getElementById("moon").innerHTML = name + moonTable[num];
+    let numD = Math.floor(Math.random() * (5 - 1) + 1);
+    const detailTable = {
+      1: " has a ring around it.",
+      2: " has large craters.",
+      3: " supports life.",
+      4: " has its own orbiting object."
+    };
+    if (numM == 1) {
+      document.getElementById("moon").innerHTML =
+        name + numTable[numM] + " The moon " + detailTable[numD];
+    } else {
+      document.getElementById("moon").innerHTML =
+        name + numTable[numM] + " One of the moons " + detailTable[numD];
+    }
   }
   moonNum();
   //generates a planet climate
   function climate() {
     let num = Math.floor(Math.random() * (6 - 1) + 1);
     const climateTable = {
-      1: "cold, the air is frigid and inhospitable.",
-      2: "cool, the landscape is covered in a layer of snow.",
-      3: "mild, the weather is temperate and landscape lush.",
-      4: "warm, the landscape is dominated by desert and savannah.",
-      5: "hot, the planet bakes in extreme heat."
+      1: "cold; the air is frigid and inhospitable.",
+      2: "cool; the landscape is covered in a layer of snow.",
+      3: "mild; the weather is temperate and landscape lush.",
+      4: "warm; the landscape is dominated by desert and savannah.",
+      5: "hot; the planet bakes in extreme heat."
     };
     document.getElementById("temp").innerHTML =
       name + "'s climate is " + climateTable[num];
@@ -172,7 +188,7 @@ function generate() {
   //generates water based off climate
   function water() {
     const waterTable = {
-      1: "'s water is all frozen.",
+      1: "'s water is all frozen as ice.",
       2: " has glaciers that feed its rivers.",
       3: " has lakes, rivers and oceans.",
       4: " has sparse fresh water.",
@@ -186,8 +202,8 @@ function generate() {
   var aqua = water();
 
   function plants() {
-    //neatned up plant gen
-    let numC = Math.floor(Math.random() * (8 - 1) + 1);
+    //generates plants if climate is right
+    let numC = Math.floor(Math.random() * (10 - 1) + 1);
     const colorTable = {
       1: " green",
       2: " red",
@@ -195,7 +211,9 @@ function generate() {
       4: " black",
       5: " grey",
       6: " yellow",
-      7: " purple"
+      7: " purple",
+      8: " orange",
+      9: " pink"
     };
     let numT = Math.floor(Math.random() * (6 - 1) + 1);
     const typeTable = {
@@ -215,37 +233,56 @@ function generate() {
       6: " are coated with slime.",
       7: " have beautiful flowers."
     };
-    //needs tidying. Lookup table isn't useful for no flora options so far.
+    let numE = Math.floor(Math.random() * (8 - 1) + 1);
+    const extraTable = {
+      1: " have medicinal properties.",
+      2: " are poisonous to humans.",
+      3: " can be burned as fuel.",
+      4: " are edible and taste delicious.",
+      5: " are edible and taste disgusting.",
+      6: " can be used to make a " + colorTable[numC] + " dye.",
+      7: " are hallucinogenic."
+    };
+    //can't get lookup table to work here but this could be neater
     if (aqua == 1) {
       document.getElementById("flora").innerHTML =
-        name + " is too cold for flora.";
+        name + " is too cold for flora. Nothing lives here.";
     } else if (aqua == 2) {
       document.getElementById("flora").innerHTML =
         name +
-        " has flora adapted to its cold climate. Such as its " +
+        " has flora adapted to its cold climate, such as its " +
         colorTable[numC] +
         typeTable[numT] +
-        ", they " +
-        detailTable[numD];
+        ". They " +
+        detailTable[numD] +
+        " These  " +
+        typeTable[numT] +
+        extraTable[numE];
     } else if (aqua == 3) {
       document.getElementById("flora").innerHTML =
         name +
         " has abundant flora, such as its " +
         colorTable[numC] +
         typeTable[numT] +
-        ", they " +
-        detailTable[numD];
+        ". They " +
+        detailTable[numD] +
+        " These  " +
+        typeTable[numT] +
+        extraTable[numE];
     } else if (aqua == 4) {
       document.getElementById("flora").innerHTML =
         name +
-        " has flora adapted to its hot climate. Such as its " +
+        " has flora adapted to its hot climate, such as its " +
         colorTable[numC] +
         typeTable[numT] +
-        ", they " +
-        detailTable[numD];
+        ". They " +
+        detailTable[numD] +
+        " These  " +
+        typeTable[numT] +
+        extraTable[numE];
     } else if (aqua == 5) {
       document.getElementById("flora").innerHTML =
-        name + " is too hot for flora.";
+        name + " is too hot for flora. Nothing lives here.";
     }
   }
   plants();
